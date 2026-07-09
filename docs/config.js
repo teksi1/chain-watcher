2a6f81f09bd2264d5d0dc5bf52b7ea25945a54a5
teksi1 <83545982+teksi1@users.noreply.github.com>
2026-07-09 16:04:08 +0300

    Update comment for appUrl in config.js

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 window.CHAIN_WATCHER_PAGES_CONFIG = Object.freeze({
-  // Paste the deployed Apps Script Web app /exec URL here.
+  // Deployed Apps Script Web app /exec URL.
   appUrl: 'https://script.google.com/macros/s/AKfycbwfX3vgrqPHVrzangE3W8W_u2u5Ckyx2r52Q_gwT2Mq26QreK_EZitYB0SjsJa8OXwP/exec',
 });
